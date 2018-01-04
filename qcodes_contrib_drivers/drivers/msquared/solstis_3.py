d33eade9eea152120e3f5ba70772b89df4f76a87
Thomas Descamps <descamps@physik.rwth-aachen.de>
2018-01-04 19:26:16 +0100

    additional methods to control the wavelength tuning (locked or not) functions to get/set lock create parameters wavelength_t/wavelength_m and lock

--- qcodes_contrib_drivers/drivers/msquared/solstis_3.py
+++ qcodes_contrib_drivers/drivers/msquared/solstis_3.py
@@ -5,6 +5,7 @@ import time
 import random
 
 from qcodes.instrument.ip import IPInstrument
+from qcodes.utils.validators import Numbers, Ints, Enum, MultiType, Anything, Strings
 
 
 class Solstis3(IPInstrument):
@@ -18,8 +19,26 @@ class Solstis3(IPInstrument):
 
         self.set_persistent(persistent)
 
-        #self.add_parameter('wavelenngth',
-                           #set_cmd)
+        self.add_parameter('wavelength_m',
+                           get_cmd = self._get_wavelength,
+                           set_cmd = self._set_wave_m,
+                           label = 'Wavelength',
+                           unit = 'nm',
+                           vals = Numbers(min_value = 700, max_value = 1000),
+                           docstring = 'wavelength locked')
+
+        self.add_parameter('wavelength_t',
+                           get_cmd = self._get_wavelength,
+                           set_cmd = self._move_wave_t,
+                           label= 'Wavelength',
+                           unit = 'nm',
+                           vals = Numbers(min_value = 700, max_value = 1000),
+                           docstring = 'wavelength not locked')
+
+        self.add_parameter('lock',
+                           get_cmd = self._is_wave_locked_m,
+                           set_cmd = self._lock_wave_m,
+                           vals = Enum(True,False))
 
     @staticmethod
     def _generate_transmission_id():
@@ -38,17 +57,62 @@ class Solstis3(IPInstrument):
         else:
             print('Connection to Solstis successful')
 
-    def _set_wavelength(self,wavelength):
+    ########## tuning WITHOUT solstis ##########
+    def _move_wave_t(self,wavelength):
+        parameters = {'wavelength':[wavelength]}
+        self.send_message('move_wave_t',parameters)
+
+    def poll_move_wave_t(self):
+        current_status = self.send_message('poll_move_wave_t')
+        inProgress = False
+        if current_status['status'][0] == 1:
+            inProgress = True
+        current_wavelength = current_status['current_wavelength'][0]
+        return inProgress, current_wavelength
+
+    def stop_move_wave_t(self):
+        self.send_message('stop_move_wave_t')
+        time.sleep(0.5) #delay between stop cmd sent and effective stop -> read final wavelength
+        return self._get_wavelength()
+
+    ########## tuning WITH solstis ##########
+    def _set_wave_m(self, wavelength):
         parameters = {'wavelength':[wavelength]}
         self.send_message('set_wave_m',parameters)
 
+    def poll_wave_m(self):
+        current_status = self.send_message('poll_wave_m')
+        inProgress = False
+        if current_status['status'][0] == 1:
+            inProgress = True
+        current_wavelength = current_status['current_wavelength'][0]
+        lock_status = current_status['lock_status'][0]
+        return inProgress, current_wavelength, lock_status
+
+    def stop_wave_m(self):
+        stop_status = self.send_message('stop_wave_m')
+        return stop_status['current_wavelength'][0]
+
+    def _lock_wave_m(self,will_be_locked):
+        if will_be_locked:
+            parameters = {'operation':'on'}
+        else:
+            parameters = {'operation':'off'}
+        locking_status = self.send_message('lock_wave_m',parameters)
+
+    def _is_wave_locked_m(self):
+        onProgress, current_wavelength, lock_status = self.poll_wave_m()
+        return lock_status
+
+    ######### read STATUS #########
     def _get_wavelength(self):
-        status = self._get_status()
+        status = self.get_status()
         return status['wavelength'][0]
 
-    def _get_status(self):
+    def get_status(self):
         return self.send_message('get_status')
 
+    ######### ASK command #########
     def send_message(self, op, parameters=None, verbose=False):
         transmission_id = self._generate_transmission_id()
 
