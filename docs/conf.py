6a182bfa3fcc365ef8aa8729ae63393513ac0e10
WilliamHPNielsen <whpn@mailbox.org>
2019-09-24 11:40:06 +0200

    Add nitpick_ignore to docs conf file

--- docs/conf.py
+++ docs/conf.py
@@ -102,3 +102,34 @@ nbsphinx_execute = 'always'
 # we mock modules that for one reason or another is not
 # there when generating the docs
 autodoc_mock_imports = ['spirack']
+
+# we allow most types from the typing modules to be used in
+# docstrings even if they don't resolve
+nitpick_ignore = [('py:class', 'Optional'),
+                  ('py:class', 'Union'),
+                  ('py:class', 'Any'),
+                  ('py:class', 'Tuple'),
+                  ('py:class', 'List'),
+                  ('py:class', 'Sequence'),
+                  ('py:class', 'Iterable'),
+                  ('py:class', 'Type'),
+                  # These are some types currently in use
+                  # in docstrings not actually defined anywhere
+                  ('py:class', 'io_manager'),
+                  ('py:class', 'chan_type'),
+                  ('py:class', 'SD_Wave'),
+                  ('py:class', 'array'),
+                  # private types that are not currently documented so links
+                  # will not resolve
+                  ('py:class', 'SweepFixedValues'),
+                  # We don't generate the docs for function since
+                  # it is deprecated
+                  ('py:class', 'Function'),
+                  # External types that for some reason or the other
+                  # don't resolve.
+                  ('py:class', 'json.encoder.JSONEncoder'),
+                  ('py:class', 'SPI_rack'),
+                  ('py:class', 'unittest.case.TestCase'),
+                  ('py:class', 'builtins.AssertionError'),
+                  ('py:exc', 'visa.VisaIOError')
+                  ]
