e8bd79291f28c523f87ffeca446d0ee42f0a6b8f
Pieter Eendebak <pieter.eendebak@gmail.com>
2019-10-30 23:41:17 +0100

    add cleanup method; use M4i driver from qcodes_contrib_drivers

--- qcodes_contrib_drivers/tests/test_Spectrum_M4i.py
+++ qcodes_contrib_drivers/tests/test_Spectrum_M4i.py
@@ -5,17 +5,18 @@ from unittest.mock import patch
 
 class TestM2j(unittest.TestCase):
 
-    @staticmethod
-    def test_M4i_wait_ready():
+    def setUp(self):
+        self.mock_pyspcm_module = MagicMock(name='pyspcm')
 
-        mock_pyspcm_module = MagicMock(name='pyspcm')
+    def test_M4i_wait_ready(self):
 
-        with patch.dict('sys.modules', pyspcm=mock_pyspcm_module):
-            import qcodes.instrument_drivers.Spectrum.M4i
-            m4i = qcodes.instrument_drivers.Spectrum.M4i.M4i('test_m4i_instrument')
-
+        with patch.dict('sys.modules', pyspcm=self.mock_pyspcm_module):
+            import qcodes_contrib_drivers.drivers.Spectrum.M4i
+            m4i = qcodes_contrib_drivers.drivers.Spectrum.M4i.M4i('test_m4i_instrument')
+            self.addCleanup(qcodes_contrib_drivers.drivers.Spectrum.M4i.M4i.close_all)
+    
             m4i.sample_rate()
-            mock_pyspcm_module.int32.assert_called()
+            self.mock_pyspcm_module.int32.assert_called()
             m4i.wait_ready()
-            mock_pyspcm_module.spcm_dwSetParam_i32.assert_called()
+            self.mock_pyspcm_module.spcm_dwSetParam_i32.assert_called()
             m4i.close()
