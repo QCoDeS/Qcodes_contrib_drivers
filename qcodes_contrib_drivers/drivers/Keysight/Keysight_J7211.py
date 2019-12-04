896db492c6b04dc8cc24a23f96d1bbb1a1754810
Joni Ikonen <joni.2.ikonen@aalto.fi>
2019-12-04 17:21:18 +0200

    Migrate from Qcodes Aalto fork

---
+++ qcodes_contrib_drivers/drivers/Keysight/Keysight_J7211.py
@@ -0,0 +1,37 @@
+from qcodes.instrument.visa import VisaInstrument
+from qcodes.utils.validators import Ints
+
+
+class Keysight_J7211(VisaInstrument):
+    r"""
+    Qcodes driver for the Keysight J7211 Attenuation Control Unit
+    Tested with J7211B
+
+    Args:
+        name: Instrument name
+        address: Address or alias of instrument
+        attenuation: Optional attenuation level to set on startup
+        **kwargs: passed to base class
+    """
+
+    def __init__(self, name, address, attenuation=None, **kwargs):
+        super().__init__(name, address, terminator='\r', **kwargs)
+
+        self.add_parameter('attenuation', unit='dB',
+                           set_cmd='ATT {:03.0f}',
+                           get_cmd='ATT?',
+                           get_parser=int)
+
+        self.connect_message()
+
+        model = self.IDN()['model']
+
+        if model in ["J7211A", "J7211B"]:
+            self.attenuation.vals = Ints(0, 120)
+        elif model in ["J7211C"]:
+            self.attenuation.vals = Ints(0, 100)
+        else:
+            raise RuntimeError("Model {} is not supported.".format(model))
+            
+        if attenuation:
+            self.attenuation(attenuation)
