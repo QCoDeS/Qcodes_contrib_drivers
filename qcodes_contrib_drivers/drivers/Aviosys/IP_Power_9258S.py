5f07bd4c25b09a72139d1ce1a908509ae0617d7d
Lukas.Lankes <l.lankes@fz-juelich.de>
2020-01-07 15:07:09 +0100

    Bug fixes, adaption to contribution guidelines

--- qcodes_contrib_drivers/drivers/Aviosys/IP_Power_9258S.py
+++ qcodes_contrib_drivers/drivers/Aviosys/IP_Power_9258S.py
@@ -1,67 +1,67 @@
 from qcodes import Instrument, InstrumentChannel, ChannelList
 from qcodes.utils.validators import Enum
 from qcodes.utils.helpers import create_on_off_val_mapping
-import urllib
+import urllib.request
 
 
 class PowerChannel(InstrumentChannel):
     """
     Channel class for a socket on the Aviosys IP Power 9258S.
+
     Args:
-        parent (Instrument): Parent instrument.
-        name (str): Channel name.
-        channel (str): Alphabetic channel id.
-    Attributes:
-        channel (str): Alphabetic channel id.
-        channel_id (int): Numeric channel id.
+        parent: Parent instrument.
+        name: Channel name.
+        channel: Alphabetic channel id.
     """
 
-    _channel_values = Enum('A', 'B', 'C', 'D')
-    _channel_ids = {'A': 1, 'B': 2, 'C': 3, 'D': 4}
+    CHANNEL_IDS = {'A': 1, 'B': 2, 'C': 3, 'D': 4}
+    CHANNEL_NAMES = Enum(*CHANNEL_IDS.keys())
 
-    def __init__(self, parent, name, channel):
+    def __init__(self, parent: Instrument, name: str, channel: str):
 
         super().__init__(parent, name)
 
         # validate the channel id
-        self._channel_values.validate(channel)
-        self.channel = channel
-        self.channel_id = self._channel_ids[channel]
+        PowerChannel.CHANNEL_NAMES.validate(channel)
+        self._id_name = channel
+        self._id_number = PowerChannel.CHANNEL_IDS[channel]
 
         # add parameters
-        self.add_parameter('power',
-                           get_cmd=self.get_power,
-                           set_cmd=self.set_power,
+        self.add_parameter('power_enabled',
+                           get_cmd=self._get_power_enabled,
+                           set_cmd=self._set_power_enabled,
                            get_parser=int,
-                           val_mapping=create_on_off_val_mapping(on_val=1, off_val=0),
-                           label='power {}'.format(self.channel))
+                           val_mapping=create_on_off_val_mapping(on_val="1", off_val="0"),
+                           label='Power {}'.format(self._id_name))
 
     # get methods
-    def get_power(self):
+    def _get_power_enabled(self):
         request = urllib.request.Request(self.parent.address+'/set.cmd?cmd=getpower')
         response = urllib.request.urlopen(request)
         request_read = response.read()
-        return request_read.decode("utf-8")[4+int(self.channel_id)*6]
+        return request_read.decode("utf-8")[4 + self._id_number * 6]
 
     # set methods
-    def set_power(self, power):
-        request = urllib.request.Request(self.parent.address+'/set.cmd?cmd=setpower+p6%s=%s' % (self.channel_id, power))
+    def _set_power_enabled(self, power):
+        request = urllib.request.Request(self.parent.address+'/set.cmd?cmd=setpower+p6%d=%s' % (self._id_number, power))
         urllib.request.urlopen(request)
 
 
 class Aviosys_IP_Power_9258S(Instrument):
     """
     Instrument driver for the Aviosys IP Power 9258 remote socket controller.
+
     Args:
-        name (str): Instrument name.
-        address (str): http address.
-        login_name (str): http login name.
-        login_password (str) http login password.
+        name: Instrument name.
+        address: http address.
+        login_name: http login name.
+        login_password http login password.
+
     Attributes:
-        address (str): http address.
+        address: http address.
     """
 
-    def __init__(self, name, address, login_name, login_password, **kwargs):
+    def __init__(self, name: str, address: str, login_name: str, login_password: str, **kwargs):
 
         super().__init__(name, **kwargs)
 
@@ -77,10 +77,10 @@ class Aviosys_IP_Power_9258S(Instrument):
 
         # add channels
         channels = ChannelList(self, "PowerChannels", PowerChannel, snapshotable=False)
-        for channel_id in ('A', 'B', 'C', 'D'):
-            channel = PowerChannel(self, 'Chan{}'.format(channel_id), channel_id)
+        for id_name in PowerChannel.CHANNEL_IDS.keys():
+            channel = PowerChannel(self, 'Chan{}'.format(id_name), id_name)
             channels.append(channel)
-            self.add_submodule(channel_id, channel)
+            self.add_submodule(id_name, channel)
         channels.lock()
         self.add_submodule("channels", channels)
 
