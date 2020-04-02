c84435234a3878d7e396240e490b3e3ad7a56993
mgunyho <marci.gunyho@gmail.com>
2020-04-02 16:06:43 +0300

    Use explicit kwargs, copy docs from RFSG

--- qcodes_contrib_drivers/drivers/NationalInstruments/PXIe_5654.py
+++ qcodes_contrib_drivers/drivers/NationalInstruments/PXIe_5654.py
@@ -1,12 +1,30 @@
+from typing import Optional
 from .RFSG import NI_RFSG
 from qcodes.utils.validators import Numbers
 
+
 class NationalInstruments_PXIe_5654(NI_RFSG):
-    """
+    r"""
     Device-specific driver for the PXIe-5654 signal generator.
+
+    Args:
+        name: Name for this instrument
+        resource: Identifier for this instrument in NI MAX.
+        dll_path: path to the NI-RFSG library DLL. If not provided, use the
+            default location,
+            ``C:\Program Files\IVI Foundation\IVI\bin\NiRFSG_64.dll``.
+        id_query: whether to perform an ID query on initialization
+        reset_device: whether to reset the device on initialization
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, name: str, resource: str,
+                 dll_path: Optional[str] = None,
+                 id_query: bool = False,
+                 reset_device: bool = False,
+                 **kwargs):
+
+        super().__init__(name=name, resource=resource, dll_path=dll_path,
+                         id_query=id_query, reset_device=reset_device,
+                         **kwargs)
 
         # device-specific parameter limits
         self.frequency.vals = Numbers(250e3, 20e9)
