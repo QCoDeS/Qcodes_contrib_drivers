184bbda08efbb80f37936c3587b62e47a874291f
Marcel Hohn <marcel.hohn@web.de>
2020-05-22 16:58:53 +0200

    add nlpc setting to driver

--- qcodes_contrib_drivers/drivers/Tektronix/Keithley_6500.py
+++ qcodes_contrib_drivers/drivers/Tektronix/Keithley_6500.py
@@ -1,4 +1,44 @@
 from qcodes.instrument.visa import VisaInstrument
+from qcodes.instrument import InstrumentChannel
+from qcodes.utils.validators import Numbers
+from functools import partial
+
+
+class Keithley_Sense(InstrumentChannel):
+    def __init__(self, parent: VisaInstrument, name: str, channel: str) -> None:
+        valid_channels = ['VOLT', 'CURR', 'RES', 'FRES']
+        if channel.upper() not in valid_channels:
+            raise ValueError('Channel must be one of the following: {}'.format(', '.join(valid_channels)))
+        super().__init__(parent, name)
+
+        self.add_parameter('measure',
+                           unit=partial(self._get_unit, channel),
+                           label=partial(self._get_label, channel),
+                           get_parser=float,
+                           get_cmd=":MEAS:{}?".format(channel),
+                           docstring="Measure value of chosen quantity (Current/Voltage/Resistance)."
+                           )
+
+        self.add_parameter('nlpc',
+                           label='NLPC',
+                           get_parser=float,
+                           get_cmd="SENS:{}:NPLC?".format(channel),
+                           set_cmd="SENS:{}:NPLC {}".format(channel, "{:.4f}"),
+                           vals=Numbers(0.0005, 12)
+                           )
+
+    @staticmethod
+    def _get_unit(channel: str) -> str:
+        channel_units = {'VOLT': 'V', 'CURR': 'A', 'RES': 'Ohm', 'FRES': 'Ohm'}
+        return channel_units[channel]
+
+    @staticmethod
+    def _get_label(channel: str) -> str:
+        channel_labels = {'VOLT': 'Measured voltage.',
+                          'CURR': 'Measured current.',
+                          'RES': 'Measured resistance',
+                          'FRES': 'Measured resistance (4w)'}
+        return channel_labels[channel]
 
 
 class Keithley_6500(VisaInstrument):
@@ -6,14 +46,11 @@ class Keithley_6500(VisaInstrument):
     def __init__(self, name: str,
                  address: str,
                  terminator="\n",
-                 reset: bool = False,
                  **kwargs):
         super().__init__(name, address, terminator=terminator, **kwargs)
 
-        self.add_parameter('sense_voltage_dc',
-                           unit='V',
-                           get_parser=float,
-                           label='Measured voltage',
-                           get_cmd=':MEAS:VOLT:DC?',
-                           docstring='Value of measured DC voltage.',
-                           )
+        for sense in ['VOLT', 'CURR', 'RES', 'FRES']:
+            channel = Keithley_Sense(self, sense.lower(), sense)
+            self.add_submodule(sense.lower(), channel)
+
+        self.connect_message()
