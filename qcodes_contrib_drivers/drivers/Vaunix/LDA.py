ae21a6bdbae366aaa0b9f43440265d35c4fb02e9
FarBo <55394589+FarBo@users.noreply.github.com>
2020-08-19 09:12:09 +0200

    fixing mypy error in linux

--- qcodes_contrib_drivers/drivers/Vaunix/LDA.py
+++ qcodes_contrib_drivers/drivers/Vaunix/LDA.py
@@ -110,7 +110,8 @@ class Vaunix_LDA(Instrument):
 
         if sys.platform != "win32":
             raise OSError(f"LDA is not supported on {sys.platform}.")
-
+        
+        assert sys.platform == "win32"
         bitness = architecture()[0]
         if "64bit" in bitness:
             full_path = os.path.join(path, "VNX_atten64")
