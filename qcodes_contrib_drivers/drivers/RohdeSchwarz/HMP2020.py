6bba51888897b44749df2451360a2787fc5143ae
Marcel Hohn <marcel.hohn@web.de>
2020-09-04 16:03:28 +0200

    add HMP driver

---
+++ qcodes_contrib_drivers/drivers/RohdeSchwarz/HMP2020.py
@@ -0,0 +1,9 @@
+from .private.HMP import _RohdeSchwarzHMP
+
+
+class RohdeSchwarzHMP4040(_RohdeSchwarzHMP):
+    """
+    This is the qcodes driver for the Rohde & Schwarz HMP2020 Power Supply
+    """
+    def __init__(self, name, address, **kwargs):
+        super().__init__(name, address, model_no=2020, **kwargs)
