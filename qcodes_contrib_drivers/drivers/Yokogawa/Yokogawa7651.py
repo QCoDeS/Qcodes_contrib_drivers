91f15a3bd71d8549b0f9e1b734e1f7b8a8d1f100
Tongyu Zhao <ty.zhao.work@gmail.com>
2022-11-10 17:57:04 -0700

    update Yokogawa7651 driver

--- qcodes_contrib_drivers/drivers/Yokogawa/Yokogawa7651.py
+++ qcodes_contrib_drivers/drivers/Yokogawa/Yokogawa7651.py
@@ -1,8 +1,6 @@
 from qcodes import VisaInstrument
-from qcodes.instrument.channel import InstrumentChannel
-from qcodes.utils.validators import Numbers, Enum
-from qcodes.utils.helpers import create_on_off_val_mapping
-import pyvisa as visa
+from typing import Dict, Optional
+import re
 
 class Yokogawa7651(VisaInstrument):
     """
@@ -18,19 +16,23 @@ class Yokogawa7651(VisaInstrument):
 
         self.add_parameter(name='output',
                            set_cmd='O{}E',
+                           get_cmd=self._get_status,
                            val_mapping={'on': 1,
                                         'off': 0})
         
         self.add_parameter(name='mode',
                            set_cmd='F{}E',
+                           get_cmd=self._get_mode,
                            val_mapping={'C.V.': 1,
                                         'C.C.': 5})
 
         self.add_parameter(name='range',
-                           set_cmd=self._set_range)
+                           set_cmd=self._set_range,
+                           get_cmd=self._get_range)
 
         self.add_parameter(name='value',
-                           set_cmd=self._set_value)
+                           set_cmd=self._set_value,
+                           get_cmd=self._get_value)
 
     def _set_range(self, range: str):
         """Function to set range based on output mode.
@@ -56,6 +58,21 @@ class Yokogawa7651(VisaInstrument):
 
         self.write(range_str+'E')
 
+    def _get_range(self):
+        """Function to get range based on return value of "OS" command.
+        """
+        range = {'F1': {'R2': '10mV',
+                        'R3': '100mV',
+                        'R4': '1V',
+                        'R5': '10V',
+                        'R6': '30V'},
+                 'F5': {'R4': '1mA',
+                        'R5': '10mA',
+                        'R6': '100mA'}}
+        response = self._setting_output()['line1']
+
+        return range[response[0]][response[1]]
+
     def _set_value(self, value: str):
         """Function to set output value.
 
@@ -72,4 +89,73 @@ class Yokogawa7651(VisaInstrument):
             polarity_str = '+'
 
         self.write('S'+polarity_str+value_str+'E')
-        
+
+    def _get_value(self):
+        """Function to get output value based on return value of "OD" command.
+        """
+        response = self._data_output()
+
+        return 1000*float(response[3])*10**(float(response[4]))
+
+    def _get_mode(self):
+        """Function to get mode based on return value of "OD" command.
+        """
+        response = self._data_output()
+
+        if response[2] == 'V':
+            return 1
+        else:
+            return 5
+
+    def _get_status(self):
+        """Function to get output status based on return value of "OC" command.
+        """
+        code = self._status_output()
+
+        return int((bin(code)[2:].zfill(8))[3])
+
+    def _status_output(self):
+        """Inquire "OC"
+        """
+        string = self.ask_raw("OC")
+
+        pattern = "STS1=(\d*)"
+        matched = re.match(pattern, string).groups()
+
+        return int(matched[0])
+
+    def _data_output(self):
+        """Inquire "OD"
+        """
+        string = self.ask_raw("OD")
+
+        pattern = "(\w)(\w\w)(\w)([\+\-\d\.]*)E([\+\-]\d*)"
+        matched = re.match(pattern, string)
+
+        return matched.groups()
+
+    def _setting_output(self):
+        """Inquire "OS"
+        """
+        self.visa_handle.write("OS", termination="\r\n")
+        line0 = self.visa_handle.read()
+        line1 = self.visa_handle.read()
+
+        response_dict = {}
+
+        pattern0 = "([A-Z]*)([\d]*)([A-Z]*)([\d\.]*)"
+        response_dict["line0"] = re.match(pattern0, line0).groups()
+
+        pattern1 = "(\w\w)(\w\w)\w([\+\-\d\.]*)E([\+\-\d]*)"
+        response_dict["line1"] = re.match(pattern1, line1).groups()
+
+        return response_dict
+
+    def get_idn(self):
+        model = self._setting_output()["line0"][1]
+        firmware = self._setting_output()["line0"][3]
+
+        IDN: Dict[str, Optional[str]] = {
+            'vendor': 'Yokogawa', 'model': model,
+            'serial': None, 'firmware': firmware}
+        return IDN
