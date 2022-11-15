bfd5b42ede546bba59ec5ee80a7524278e292742
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2022-11-15 14:53:09 +0100

    restore module for backwards compatibility

---
+++ qcodes_contrib_drivers/drivers/CMTS5048.py
@@ -0,0 +1,6 @@
+import warnings
+from qcodes_contrib_drivers.drivers.CopperMountain.S5048 import _unit_map, CMTIntParser, TraceNotReady, CMTS5048Trace, CMTS5048
+warnings.warn(
+    "Please import CopperMountain S5048 driver from qcodes_contrib_drivers.drivers.CopperMountain.S5048 "
+    "This module will be removed  in a future release."
+    )
