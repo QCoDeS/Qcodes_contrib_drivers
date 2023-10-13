48073e20a4eeda97104bf0bdbf68b4f38b42922b
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2023-10-13 16:32:25 +0200

    Autoformat and sort imports:

--- qcodes_contrib_drivers/drivers/M2/M2_Solstis_3.py
+++ qcodes_contrib_drivers/drivers/M2/M2_Solstis_3.py
@@ -1,44 +1,43 @@
-import socket
 import json
-import threading
-import time
 import random
+import time
 
 from qcodes.instrument.ip import IPInstrument
-from qcodes.utils.validators import Numbers, Ints, Enum, MultiType, Anything, Strings
+from qcodes.utils.validators import Numbers, Enum
 
 
 class M2Solstis3(IPInstrument):
     def __init__(self, name, address=None, port=None, timeout=5, controller_address=None,
-                  persistent=True, write_confirmation=True, testing=False,**kwargs):
+                 persistent=True, write_confirmation=True, testing=False, **kwargs):
 
         super().__init__(name, address=address, port=port, timeout=timeout,
-                         persistent=False, write_confirmation=write_confirmation, terminator='', **kwargs)
+                         persistent=False, write_confirmation=write_confirmation, terminator='',
+                         **kwargs)
 
         self._controller_address = controller_address
 
         self.set_persistent(persistent)
 
         self.add_parameter('wavelength_m',
-                           get_cmd = self._get_wavelength,
-                           set_cmd = self._set_wave_m,
-                           label = 'Wavelength',
-                           unit = 'nm',
-                           vals = Numbers(min_value = 700, max_value = 1000),
-                           docstring = 'wavelength locked')
+                           get_cmd=self._get_wavelength,
+                           set_cmd=self._set_wave_m,
+                           label='Wavelength',
+                           unit='nm',
+                           vals=Numbers(min_value=700, max_value=1000),
+                           docstring='wavelength locked')
 
         self.add_parameter('wavelength_t',
-                           get_cmd = self._get_wavelength,
-                           set_cmd = self._move_wave_t,
-                           label= 'Wavelength',
-                           unit = 'nm',
-                           vals = Numbers(min_value = 700, max_value = 1000),
-                           docstring = 'wavelength not locked')
+                           get_cmd=self._get_wavelength,
+                           set_cmd=self._move_wave_t,
+                           label='Wavelength',
+                           unit='nm',
+                           vals=Numbers(min_value=700, max_value=1000),
+                           docstring='wavelength not locked')
 
         self.add_parameter('lock',
-                           get_cmd = self._is_wave_locked_m,
-                           set_cmd = self._lock_wave_m,
-                           vals = Enum(True,False))
+                           get_cmd=self._is_wave_locked_m,
+                           set_cmd=self._lock_wave_m,
+                           vals=Enum(True, False))
 
     @staticmethod
     def _generate_transmission_id():
@@ -58,9 +57,9 @@ class M2Solstis3(IPInstrument):
             print('Connection to Solstis successful')
 
     ########## tuning WITHOUT solstis ##########
-    def _move_wave_t(self,wavelength):
-        parameters = {'wavelength':[wavelength]}
-        self.send_message('move_wave_t',parameters)
+    def _move_wave_t(self, wavelength):
+        parameters = {'wavelength': [wavelength]}
+        self.send_message('move_wave_t', parameters)
 
     def poll_move_wave_t(self):
         current_status = self.send_message('poll_move_wave_t')
@@ -72,13 +71,13 @@ class M2Solstis3(IPInstrument):
 
     def stop_move_wave_t(self):
         self.send_message('stop_move_wave_t')
-        time.sleep(0.5) #delay between stop cmd sent and effective stop -> read final wavelength
+        time.sleep(0.5)  # delay between stop cmd sent and effective stop -> read final wavelength
         return self._get_wavelength()
 
     ########## tuning WITH solstis ##########
     def _set_wave_m(self, wavelength):
-        parameters = {'wavelength':[wavelength]}
-        self.send_message('set_wave_m',parameters)
+        parameters = {'wavelength': [wavelength]}
+        self.send_message('set_wave_m', parameters)
 
     def poll_wave_m(self):
         current_status = self.send_message('poll_wave_m')
@@ -93,12 +92,12 @@ class M2Solstis3(IPInstrument):
         stop_status = self.send_message('stop_wave_m')
         return stop_status['current_wavelength'][0]
 
-    def _lock_wave_m(self,will_be_locked):
+    def _lock_wave_m(self, will_be_locked):
         if will_be_locked:
-            parameters = {'operation':'on'}
+            parameters = {'operation': 'on'}
         else:
-            parameters = {'operation':'off'}
-        locking_status = self.send_message('lock_wave_m',parameters)
+            parameters = {'operation': 'off'}
+        locking_status = self.send_message('lock_wave_m', parameters)
 
     def _is_wave_locked_m(self):
         onProgress, current_wavelength, lock_status = self.poll_wave_m()
@@ -141,4 +140,3 @@ class M2Solstis3(IPInstrument):
         snapshot['controller_address'] = self._controller_address
 
         return snapshot
-
