f7958add7d87607250ab3fffaa394e1d3f95a662
Vyron Vasileiads <hi@fedonman.com>
2023-11-15 20:33:44 +0100

    Update Valon5015

--- qcodes_contrib_drivers/drivers/Valon/Valon_5015.py
+++ qcodes_contrib_drivers/drivers/Valon/Valon_5015.py
@@ -1,16 +1,32 @@
+"""
+Driver for Valon 5015 Frequency Synthesizer.
+
+Please refer to Valon's 5015 Frequency Synthesizer manual for further
+details and functionality. This model is not SCPI compliant.
+"""
+import re
+import logging
+from typing import Any
+
 from qcodes.utils.validators import Ints, Numbers, Bool
 from qcodes import VisaInstrument
-from qcodes.parameters import create_on_off_val_mapping
-from typing import Any
-import re
 import pyvisa.constants as vi_const
 
+
+log = logging.getLogger(__name__)
+
+
 class Valon5015(VisaInstrument):
-    __frequency_regex = re.compile(r"F (?P<frequency>\d+[.]\d+) MHz")
-    __offset_regex = re.compile(r"OFFSET (?P<offset>\d+[.]\d+) MHz")
+    """Driver for Valon 5015 Frequency Synthesizer.
+
+    This driver does not contain all commands available for the Valon 5015 but
+    only the ones most commonly used.
+    """
+    __frequency_regex = re.compile(r"F (?P<frequency>\d+([.]\d+)?) MHz")
+    __offset_regex = re.compile(r"OFFSET (?P<offset>\d+([.]\d+)?) MHz")
     __power_regex = re.compile(r"PWR (?P<power>\d+[.]\d+)")
     __modulation_db_regex = re.compile(r"AMD (?P<modulation_db>\d+[.]\d+) dB")
-    __modulation_frequency_regex = re.compile(r"AMF (?P<modulation_frequency>\d+[.]\d+) kHz")
+    __modulation_frequency_regex = re.compile(r"AMF (?P<modulation_frequency>\d+([.]\d+)?) kHz")
     __low_power_mode_enabled_regex = re.compile(r"PDN (?P<low_power_mode_enabled>[01])")
     __buffer_amplifiers_enabled_regex = re.compile(r"OEN (?P<buffer_amplifiers_enabled>[01])")
 
@@ -19,59 +35,68 @@ class Valon5015(VisaInstrument):
 
         self.add_parameter(name='status',
                            label='Status',
-                           get_cmd=self._get_status)
+                           get_cmd=self._get_status,
+                           docstring="Get the manufacturer's name, model, serial number, firmware version, firmware build date and time, uP clock rate, power supply voltage (VBAT), internal temperature, LM, and UID.")
         
         self.add_parameter(name='id',
                            label='ID',
                            get_cmd=self._get_id,
                            set_cmd=self._set_id,
-                           vals=Ints(0))
+                           vals=Ints(0),
+                           docstring="Get an identifying string containing the following information: Valon Technology, 5015/5019, serial number, firmware revision. Setting a number to the `id` method will flash the `Status LED` in short burst of 1 second for the number of times entered.")
 
         self.add_parameter(name='frequency',
                            label='Frequency',
                            unit='Hz',
                            get_cmd=self._get_frequency,
                            set_cmd=self._set_frequency,
-                           vals=Numbers(1, 15e9))
+                           vals=Numbers(10e6, 15e9),
+                           docstring="Get/set the frequency of the single tone. The allowed range is from 10 MHz to 15 GHz and the value is expressed in Hz.")
         
         self.add_parameter(name='offset',
                            label='Offset',
                            unit='Hz',
                            get_cmd=self._get_offset,
                            set_cmd=self._set_offset,
-                           vals=Numbers(-4295e3, 4295e3))
+                           vals=Numbers(-4295e3, 4295e3),
+                           docstring="Get/set the offset to be added or substracted from the frequency. The allowed range is from -4.295 GHz to 4295 GHz and the value is expressed in Hz.")
         
         self.add_parameter(name='power',
                            label='Power',
                            unit='dBm',
                            get_cmd=self._get_power,
-                           set_cmd=self._set_power)
+                           set_cmd=self._set_power,
+                           docstring="Get/Set the power level in dBm.")
         
         self.add_parameter(name='modulation_db',
                            label='Modulation_dB',
                            unit='dB',
                            get_cmd=self._get_modulation_db,
                            set_cmd=self._set_modulation_db,
-                           vals=Numbers(0.0))
+                           vals=Numbers(0.0),
+                           docstring="Get/Set the AM modulation in dB. A value of 0 dB disables the AM modulation.")
         
         self.add_parameter(name='modulation_frequency',
                            label='Modulation_Frequency',
                            unit='Hz',
                            get_cmd=self._get_modulation_frequency,
                            set_cmd=self._set_modulation_frequency,
-                           vals=Numbers(1, 2e3))
+                           vals=Numbers(1, 2e3),
+                           docstring="Get/Set the AM modulation frequency. The allowed range is from 1 Hz to 2 kHz and the value is expressed in Hz.")
 
         self.add_parameter(name='low_power_mode_enabled',
                            label='Low Power Mode Enabled',
                            get_cmd=self._get_low_power_mode_enabled,
                            set_cmd=self._set_low_power_mode_enabled,
-                           vals=Bool())
+                           vals=Bool(),
+                           docstring="Enables or disables the low power mode.")
         
         self.add_parameter(name='buffer_amplifiers_enabled',
                            label='Buffer Amplifiers Enabled',
                            get_cmd=self._get_buffer_amplifiers_enabled,
                            set_cmd=self._set_buffer_amplifiers_enabled,
-                           vals=Bool())
+                           vals=Bool(),
+                           docstring="Enables or disables the RF output buffer amplifiers.")
 
     def _get_status(self):
         responses = [self.ask("stat") for _ in range(14)]
