65b888f024223898308b364bd2b035fa474423b6
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2024-01-31 10:03:50 +0100

    fix syntax warnings

--- src/qcodes_contrib_drivers/drivers/QuTech/IVVI.py
+++ src/qcodes_contrib_drivers/drivers/QuTech/IVVI.py
@@ -167,7 +167,7 @@ class IVVI(VisaInstrument):
         print('Initialized IVVI-rack in %.2fs' % (t1 - t0))
 
     def get_idn(self):
-        """
+        r"""
         Overwrites the get_idn function using constants as the hardware
         does not have a proper \*IDN function.
         """
