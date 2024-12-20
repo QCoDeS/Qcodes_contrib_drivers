4a836f59683a1eb91a87f7537aa1de896a360d97
DCEM <git-fork@6xg.de>
2024-12-20 01:16:55 +0100

    correct name and location of IntegratedStepperMotorCLI classes

--- src/qcodes_contrib_drivers/drivers/Thorlabs/private/DotNetAPI/IntegratedStepperMotorsCLI.py
+++ src/qcodes_contrib_drivers/drivers/Thorlabs/private/DotNetAPI/IntegratedStepperMotorsCLI.py
@@ -1,8 +1,16 @@
 import logging
 
-from .IntegratedStepperMotorsCLI_IntegratedStepperMotor import IntegratedStepperMotor
+from .GenericMotorCLI_AdvancedMotor import GenericAdvancedMotorCLI
 
 log = logging.getLogger(__name__)
+
+
+class IntegratedStepperMotor(GenericAdvancedMotorCLI):
+    """
+    This class is the common base class to all Integrated Stepper Controls.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class CageRotator(IntegratedStepperMotor):
