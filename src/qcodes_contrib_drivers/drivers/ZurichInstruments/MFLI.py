667c3ed1a3a179e93868ba043161216690370ca0
Julien Barrier <1346858+julienbarrier@users.noreply.github.com>
2025-11-27 09:42:18 +0100

    PEP8 + mypy checked

--- src/qcodes_contrib_drivers/drivers/ZurichInstruments/MFLI.py
+++ src/qcodes_contrib_drivers/drivers/ZurichInstruments/MFLI.py
@@ -4,9 +4,13 @@ from typing import Any, Optional
 from qcodes import Parameter
 from zhinst.qcodes import MFLI as mfli
 
+
 class ComplexSampleParameter(Parameter):
     def __init__(
-        self, *args: Any, dict_parameter: Optional[Parameter] = None, **kwargs: Any
+        self,
+        *args: Any,
+        dict_parameter: Optional[Parameter] = None,
+        **kwargs: Any
     ):
         super().__init__(*args, **kwargs)
         if dict_parameter is None:
@@ -16,12 +20,13 @@ class ComplexSampleParameter(Parameter):
     def get_raw(self) -> ParamRawDataType:
         values_dict = self._dict_parameter.get()
         return complex(values_dict["x"], values_dict["y"])
-        
+
+
 class MFLI(mfli):
     """
-    This wrapper adds back a "complex sample" parameter to the demodulators such that
-    we can use them in the way that we have done with "sample" parameter
-    in version 0.2 of ZHINST-qcodes
+    This wrapper adds back a "complex sample" parameter to the demodulators
+    such that we can use them in the way that it was done with "sample"
+    parameter in version 0.2 of ZHINST-qcodes
     written by jenshnielsen: https://github.com/zhinst/zhinst-qcodes/issues/41
     """
 
