eb2f1e53b02fef9e7bd46609acc4dfe6d8acbb49
Julien Barrier <1346858+julienbarrier@users.noreply.github.com>
2025-12-01 09:36:32 +0100

    add functions

--- src/qcodes_contrib_drivers/drivers/QuantumDesign/Opticool.py
+++ src/qcodes_contrib_drivers/drivers/QuantumDesign/Opticool.py
@@ -10,7 +10,7 @@ Authors:
 
 import logging
 from typing import Optional, Any
-import MultiPyVu as mpv
+from  MultiPyVu import Client
 
 from qcodes.instrument.base import Instrument
 from qcodes.parameters import Parameter
@@ -25,11 +25,12 @@ class Opticool(Instrument):
 
     Args:
         name (str): name for the instrument
-        address (str): IP address of the MultiPyVu server instance
+        address (str): IP address of the MultiPyVu server instance. Defaults to
+            localhost
         port (int): port to connect IP. Defaults to 5000
-        
+
     Status: work-in-progress
-    
+
     Todo:
         write down all the _functions and test driver
         check parsing
@@ -40,12 +41,12 @@ class Opticool(Instrument):
     def __init__(
             self,
             name: str,
-            address: str,
+            address: Optional[str] = 'localhost',
             port: Optional[int] = 5000,
             **kwargs: Any) -> None:
         super().__init__(name, **kwargs)
-        self.client = mpv.Client(host=address, port=port)
-        
+        self.client = Client(host=address, port=port)
+
         self.temperature_shield = Parameter(
             name='temperature_shield',
             label='Shield temperature',
@@ -54,7 +55,7 @@ class Opticool(Instrument):
             get_parser=float,
             instrument=self
         )
-        
+
         self.temperature_4k_plate = Parameter(
             name='temperature_4k_plate',
             label='4K plate temperature',
@@ -63,7 +64,7 @@ class Opticool(Instrument):
             get_parser=float,
             instrument=self
         )
-        
+
         self.temperature_magnet = Parameter(
             name='temperature_magnet',
             label='Magnet temperature',
@@ -72,7 +73,7 @@ class Opticool(Instrument):
             get_parser=float,
             instrument=self
         )
-        
+
         self.temperature_sample = Parameter(
             name='temperature_sample',
             label='Sample temperature',
@@ -83,7 +84,7 @@ class Opticool(Instrument):
             vals=vals.Numbers(1.4, 350),
             instrument=self
         )
-        
+
         self.temperature_ramp_method = Parameter(
             name='temperature_ramp_method',
             label='Ramp method for the sample stage temperature',
@@ -92,7 +93,7 @@ class Opticool(Instrument):
             val_mapping=,#TODO
             instrument=self
         )
-        
+
         self.temperature_ramp_rate = Parameter(
             name='temperature_ramp_rate',
             label='Temperature ramp rate for the sample stage',
@@ -101,7 +102,7 @@ class Opticool(Instrument):
             set_cmd=self._set_temperature_ramp_rate,
             instrument=self
         )
-        
+
         self.heater_power_4k_plate = Parameter(
             name='heater_power_4k_plate',
             label='Heater power at the 4K plate',
@@ -110,7 +111,7 @@ class Opticool(Instrument):
             get_parser=float,
             instrument=self
         )
-        
+
         self.heater_power_sample = Parameter(
             name='heater_power_sample',
             label='Heater power at the sample plate',
@@ -119,62 +120,62 @@ class Opticool(Instrument):
             get_parser=float,
             instrument=self
         )
-        
+
         self.status_cryostat = Parameter(
             name='status_cryostat',
             label='Cryostat status mode',
             get_cmd=self._get_status_cryostat,
             instrument=self
         )
-        
+
         self.status_magnet = Parameter(
             name='status_magnet',
             label='Magnet status',
             get_cmd=self._get_status_magnet,
             instrument=self
         )
-        
+
         self.status_compressor = Parameter(
             name='status_compressor',
             label='Compressor status',
             get_cmd=self._get_status_compressor,
             instrument=self,
         )
-        
+
         self.status_cooler = Parameter(
             name='status_cooler',
             label='Cooler status',
             get_cmd=self._get_status_cooler,
             instrument=self
         )
-        
+
         self.status_liquid = Parameter(
             name='status_liquid',
             label='Status of liquid in cooling chamber',
             get_cmd=self._get_status_liquid,
             instrument=self
         )
-        
+
         self.status_temperature = Parameter(
             name='status_temperature',
             label='Status of temperature control',
             get_cmd=self._get_status_temperature,
             instrument=self
         )
-        
+
         self.pressure_cooler = Parameter(
             name='pressure_cooler',
             label='Pressure in the circulation loop',
             get_cmd=self._get_pressure_cooler,
             instrument=self
         )
-        
+
         self.pressure_vacuum_chamber = Parameter(
             name='pressure_vacuum_chamber',
             label='Vacuum chamber pressure',
             get_cmd=self._get_pressure_vacuum_chamber
         )
-        
+
         self.magnet_field = Parameter(
             name='magnet_field',
             label='Magnetic field',
@@ -186,11 +187,11 @@ class Opticool(Instrument):
             vals=vals.Numbers(min_value=-7, max_value=7),
             instrument=self,
         )
-        
+
         self.magnet_ramp_rate = Parameter(
             name='magnet_ramp_rate',
             label='Magnet ramp rate',
-            unit='T/min',
+            unit='T/s',
             get_cmd=self._get_magnet_ramp_rate,
             get_parser=self._parse_oersted_to_tesla,
             set_cmd=self._set_magnet_ramp_rate,
@@ -198,7 +199,7 @@ class Opticool(Instrument):
             vals=vals.Numbers(min_value=0, max_value=1), #TODO - change values
             instrument=self,
         )
-        
+
         self.magnet_ramp_method = Parameter(
             name='magnet_ramp_method',
             label='Magnet ramp methode',
@@ -211,9 +212,45 @@ class Opticool(Instrument):
         self.client.open()
         self.connect_message()
 
-    def ramp_field(self, target, rate):
-        self.client.set_field(target, rate)
-        
+    def ramp_field(self):
+        self.client.set_field(
+            self._field_target,
+            self._field_rate,
+            self._field_approach_mode,
+        )
+        #TODO - define _field_target and _field_rate
+
+    def ramp_temp(self):
+        self.client.set_temperature(
+            self._temp_setpoint,
+            self._temp_rate,
+            self._temp_approach_mode,
+        )
+
+    def seal(self):
+        self.client.set_chamber(client.chamber.mode.seal)
+        #TODO - logging message
+
+    def purge_seal(self):
+        self.client.set_chamber(client.chamber.mode.purge_seal)
+        #TODO - logging message
+
+    def vent_seal(self):
+        self.client.set_chamber(client.chamber.mode.vent_seal)
+        #TODO - logging message
+
+    def pump_continuous(self):
+        self.client.set_chamber(client.chamber.mode.pump_continuous)
+        #TODO - logging message
+
+    def vent_continuous(self):
+        self.client.set_chamber(client.chamber.mode.vent_continuous)
+        #TODO - logging message
+
+    def high_vacuum(self):
+        self.client.set_chamber(client.chamber.mode.high_vacuum)
+        #TODO - logging message
+
     def close(self) -> None:
         """Close the connection"""
         self.log.debug(f"close connection to {self.name}")
@@ -222,86 +259,101 @@ class Opticool(Instrument):
     def get_idn(self) -> dict[str, str | None]:
         """Return the Instrument Identifier Message"""
         #TODO
-        
+
     def _get_temperature_shield():
         #TODO
-        
+
     def _get_temperature_4k_plate():
         #TODO
-        
+
     def _get_temperature_magnet():
         #TODO
-        
+
     def _get_temperature_sample():
         T, _ = self.client.get_temperature()
         return T
-        
+
     def _get_temperature_ramp_method():
         #TODO
-        
+
     def _get_temperature_ramp_rate():
         #TODO
-        
+
     def _get_heater_power_4k_plate():
         #TODO
-        
+
     def _get_heater_power_sample():
         #TODO
-        
+
     def _get_status_cryostat():
         #TODO
-        
+
     def _get_status_magnet():
         _, sB = self.client.get_field()
         return sB
-        
+
     def _get_status_compressor():
         #TODO
-        
+
     def _get_status_cooler():
         #TODO
-        
+
     def _get_status_liquid():
         #TODO
-        
+
+    def _get_status_chamber(self):
+        return client.get_chamber()
+
     def _get_status_temperature():
         _, sT = self.client.get_temperature()
         return sT
-        
+
     def _get_pressure_cooler():
         #TODO
-        
+
     def _get_pressure_vacuum_chamber():
         #TODO
-        
+
     def _get_magnet_field():
         B, _ = self.client.get_field()
-        
+
     def _get_magnet_ramp_rate():
         #TODO
-        
+
     def _get_magnet_ramp_method():
         #TODO
-        
-    def _set_temperature_sample():
-        T, sT = self.client.get_temperature()
-        #TODO
-        
+
+    def _set_temperature_sample(self, set_point):
+        _, rate, approach = self.client.get_temperature_setpoints()
+        self.client.set_temperature(set_point, rate, approach)
+
     def _set_temperature_ramp_method():
+        temperature, rate, _ = self.client.get_temperature_setpoints()
         #TODO
-        
+        """these two are not correct:
+        I need to write down a _temp_ramp_approach dict, that is save when one
+        sets the temperature. then a specific function start_temperature_ramp
+        """
+
     def _set_temperature_ramp_rate():
         #TODO
-        
+
+    def _set_status_chamber(self, status):
+        """accept items seal, purge_seal, vent, vent_seal, pump_continuous,
+        vent_continuous and high_vacuum.
+        """
+        client.set_chamber()
+
+
     def _set_magnet_field():
         #TODO
-        
+
     def _set_magnet_ramp_rate():
         #TODO
-        
+
     def _set_magnet_ramp_method():
         #TODO
-    
+
 
     def _parse_oersted_to_tesla(val):
         return float(val)*1e-4
