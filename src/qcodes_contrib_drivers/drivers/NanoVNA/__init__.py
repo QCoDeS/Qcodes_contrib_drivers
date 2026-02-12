f72a8fe9eff223c7f38ccdf7193b8d13acbec72c
Laird <lairde@lancaster.ac.uk>
2026-02-12 12:48:43 +0000

    Update __init__.py

--- src/qcodes_contrib_drivers/drivers/NanoVNA/__init__.py
+++ src/qcodes_contrib_drivers/drivers/NanoVNA/__init__.py
@@ -1,4 +1,4 @@
 try:
-    import pynovavna
+    import pynanovna
 except ImportError as e:
     raise ImportError("To use this driver, install pynovavna by following instructions at https://github.com/PICC-Group/pynanovna") from e
